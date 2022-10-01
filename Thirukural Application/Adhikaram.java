fc55953232bb1324773b581dcd84f1a3d111cef0
Antany Tom Felix S <112949397+Antany-99@users.noreply.github.com>
2022-10-01 11:32:36 +0530

    Add files via upload

---
+++ Thirukural Application/Adhikaram.java
@@ -0,0 +1,25 @@
+package Thirukural;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+public class Adhikaram {
+	public static void One(File  fileOne) throws IOException  {
+		Scanner s=new Scanner (System.in);
+		System.out.println("Enter the ADHIGARAM Number You Want");
+		int Adhigaram=s.nextInt();
+		int a=Adhigaram*22;
+		FileReader r=new FileReader(fileOne);
+		BufferedReader Read=new BufferedReader(r);
+		String Line=Read.readLine();
+		int LineCount=0;
+		for(int i=0;i<a;i++) {
+			if(LineCount>=(a-22)) {System.out.print(Line);
+				System.out.println();}
+			Line=Read.readLine();
+			LineCount++;}	
+	}
+}
+
---
+++ Thirukural Application/MainClassss.java
@@ -0,0 +1,31 @@
+package Thirukural;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+public class MainClassss{
+public static void main(String[] args) throws IOException{
+	long start=System.currentTimeMillis();
+		Adhikaram Zero=new Adhikaram();
+		Kural k=new Kural();
+		thiru z=new thiru();
+		int cont=0;
+		Scanner Thiru=new Scanner(System.in);
+		File fileOne=new File("C:\\Adhigaram.txt");
+		File fileTwo=new File("C:\\Kural.txt");
+		z.adhi(fileOne);
+		z.kurals(fileTwo);
+		while(cont==0) {
+		System.out.println("If you want Adhigaram enter 1 or if you want particular Kural enter 2");
+		int get=Thiru.nextInt();
+			switch(get){
+				case 1:Zero.One(fileOne);break;
+				case 2:k.Two(fileTwo);break;}
+			System.out.println("If You Want to Continue enter 0 or break enter 1");
+			cont=Thiru.nextInt();}
+		long end=System.currentTimeMillis();
+		System.out.println(end-start);
+	}	
+} 
